3fdfcb308e51df9b8bd87e6d2082eba187105285
mostafa nematpour <nematpor46@gmail.com>
2023-05-19 15:39:16 +0330

    quick-sort

---
+++ algorithms-code/sort/02-quick-sort.py
@@ -0,0 +1,11 @@
+def quick_sort(arr):
+    if (len(arr) < 2):
+        return arr
+    else:
+        pivotal_item = arr[0]
+        smaller_arr = [i for i in arr[1:] if i <= pivotal_item]
+        greater_arr = [i for i in arr[1:] if i > pivotal_item]
+    return quick_sort(smaller_arr) + [pivotal_item] + quick_sort(greater_arr)
+
+
+print(quick_sort([99,98,1,5,2,3,4,98,5,5,0,586,56,3,1,1,1,1,1,2,3,57]))

1e4bca8d17ce767f28396f2eecf12d644b45ccb0
mostafa nematpour <nematpor46@gmail.com>
2023-05-21 18:36:16 +0330

    breadth-first-search-{bfs}

---
+++ algorithms-code/search/02-breadth-first-search.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+
+def node_is_target(node):
+    return node[-1] == 'x'
+
+
+def bfs(graph, start_node):
+    search_queue = deque()
+    search_queue += graph[start_node]
+
+    searched = []
+    while search_queue:
+        node = search_queue.popleft()
+        if not node in searched:
+            if node_is_target(node):
+                return node
+            elif node in graph:
+                search_queue += graph[node]
+    return False
+
+
+graph = {}
+
+graph["me"] = ["mohammad", "ali", "parsa"]
+
+graph["mohammad"] = ["mohammad-ali", "mahdi"]
+graph["ali"] = ["mohammad", "robert"]
+graph["parsa"] = ["mostafa", "my", "maria", "max"]
+
+graph["mohammad-ali"] = []
+graph["mahdi"] = []
+graph["robert"] = []
+graph["mostafa"] = []
+graph["my"] = []
+graph["max"] = []
+
+print(bfs(graph, "me"))

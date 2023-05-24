ab7842b955c6a2172029d3557d00b59693231346
mostafa-nematpour <nematpor46@gmail.com>
2023-05-24 11:12:11 +0430

    dijkstra

---
+++ algorithms-code/graph/02-dijkstra.py
@@ -0,0 +1,22 @@
+graph = {}
+
+graph["start"] = {}
+graph["start"]["a"] = 6
+graph["start"]["b"] = 2
+
+graph["a"] = {}
+graph["a"]["fin"] = 1
+
+graph["b"] = {}
+graph["b"]["a"] = 3
+graph["b"]["fin"] = 5
+
+graph["fin"] = {}
+
+
+parents = {}
+parents["a"] = "start"
+parents["b"] = "start"
+parents["fin"] = None
+
+print(graph)

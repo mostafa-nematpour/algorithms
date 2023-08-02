5de89de29fbf11cd82765d899972838106f835c6
mostafa nematpour <nematpor46@gmail.com>
2023-08-02 19:25:02 +0330

    add graph

---
+++ algorithms-code/data-structure-sample/graph/graph.py
@@ -0,0 +1,11 @@
+# i think it is not a good idea
+
+graph = {}
+
+graph["you"] = ["alice", "bob", "claire"]
+graph["bob"] = ["anuj", "peggy"]
+graph["alice"] = ["peggy"]
+graph["claire"] = ["thom","jonny"]
+graph["anuj"] = []
+graph["thom"] = []
+graph["jonny"] = []

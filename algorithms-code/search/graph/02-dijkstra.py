c262b9e75fffeda32b54ec8d6c5804bc6b31b6e6
mostafa nematpour <nematpor46@gmail.com>
2023-08-03 21:17:48 +0330

    update dijkstra

--- algorithms-code/search/graph/02-dijkstra.py
+++ algorithms-code/search/graph/02-dijkstra.py
@@ -1,22 +1,47 @@
-graph = {}
 
+graph = {}
 graph["start"] = {}
 graph["start"]["a"] = 6
 graph["start"]["b"] = 2
-
 graph["a"] = {}
 graph["a"]["fin"] = 1
-
 graph["b"] = {}
 graph["b"]["a"] = 3
 graph["b"]["fin"] = 5
-
 graph["fin"] = {}
 
+infinity = float("inf")
+costs = {}
+costs["a"] = 6
+costs["b"] = 2
+costs["fin"] = infinity
 
 parents = {}
 parents["a"] = "start"
 parents["b"] = "start"
 parents["fin"] = None
 
-print(graph)
+processed = []
+
+
+def find_lower_cost_node(costs):
+    lowest_cost = float("inf")
+    lowest_cost_node = None
+    for node in costs:
+        cost = costs[node]
+        if cost < lowest_cost and node not in processed:
+            lowest_cost = cost
+            lowest_cost_node = node
+    
+
+node = find_lower_cost_node(costs)
+while node is not None:
+    costs = costs[node]
+    neighbors = graph[node]
+    for n in neighbors.keys():
+        new_cost = cost + neighbors[n]
+        if costs[n] > new_cost:
+            costs[n] = new_cost
+            parents[n] = node
+    processed.append(node)
+    node = find_lower_cost_node(costs)
